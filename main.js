200b0e8bd1d802f55bfc6465d29ef87a70818d77
Kaleb Dykema <dykemakaleb@gmail.com>
2022-03-20 20:51:56 -0500

    Updated textures and added new enemy sprite

--- main.js
+++ main.js
@@ -2,15 +2,48 @@
 import "./style.scss";
 import kaboom from "kaboom";
 
+// Vars and helper functions
+const numScale = 5;
+
+// Scale a number based on numScale, defined above
+const s = (n = 1) => n * numScale;
+
 // Initalize Kaboom
 kaboom();
 
 // Load sprites
 loadSprite("purpleman", "./assets/purpleman.png");
+loadSprite("greenman", "./assets/greenman.png");
+loadSprite("grass", "./assets/grass.png");
 
-// Add to canvas
+// Add sprite to canvas
 add([
 	sprite("purpleman"),
-	scale(2),
-	pos(20,20)
+	pos(s(20),s(20)),
+	body(),
+	area(),
+	scale(s())
 ]);
+
+// Load a custom level
+addLevel([
+	"     ",
+	"  @  ",
+	"     ",
+	"xxxxx"
+], {
+	width: s(32),
+	height: s(32),
+	"x": () => [
+		sprite("grass"),
+		area(),
+		solid(),
+		scale(s())
+	],
+	"@": () => [
+		sprite("greenman"),
+		body(),
+		area(),
+		scale(s())
+	]
+});
